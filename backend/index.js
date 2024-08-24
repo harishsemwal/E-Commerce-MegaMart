0a228e4524a9f85477d938070eebe0eaffd6c764
Harishsemwal <harishsemwal581@gmail.com>
2024-08-24 13:39:57 +0530

    Connection With Database
    Connection With Database

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,105 @@ const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 
-
 app.use(express.json());
 app.use(cors());
 
-// Connect to MongoDB
+mongoose.connect("mongodb+srv://harishsemwal:Megamart2024@cluster0.37bjgvi.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB successfully');
+}).catch((error) => {
+    console.log('Error in connecting to MongoDB:', error);
+});
 
+// API Creation
+app.get("/", (req, res) => {
+    res.send("Express app is Running")
+});
 
+app.listen(port, (error) => {
+    if(!error) {
+        console.log(`Server is running on port ${port}`);
+    }
+    else{
+        console.log("Error in connecting to MongoDB", error);
+    }
+});
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.filename}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage : storage });
+
+// Creating upload endpoint for image storage
+app.use('/images', express.static('upload/images'));
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+// Schema for Creating Products
+
+const Product = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now(),
+    },
+    avilable:{
+        type: Boolean,
+        default: true,
+    }
+})
+
+// Creating a new product
+
+app.post('/addproduct', async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+        message: "Product saved successfully",
+    })
+})
